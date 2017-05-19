605b7d7342cb0d114404a79e890ed9bdcf7769ef
Katherine Dix <kathdix@seas.upenn.edu>
2017-05-19 12:56:00 -0400

    creating files

---
+++ game_model.py
@@ -0,0 +1,31 @@
+class Board(object):
+
+    def __init__(self, dimension="large"):
+        pass
+
+    def add_word(self, word, start, direction):
+        pass
+
+
+class Square(object):
+
+    def __init__(self, type=None):
+        pass
+
+    def add_tile(self, tile):
+        pass
+
+
+class Lexicon(object):
+
+    def __init__(self):
+        pass
+
+    def add(self):
+        pass
+
+    def get(self):
+        pass
+
+    def search(self):
+        pass
